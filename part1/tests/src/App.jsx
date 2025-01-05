cbce3ef0ca3b59a0d126bb466b466ce0d830c2a1
Nicolás Gamarra <nicolas.gamarra@fing.edu.uy>
2025-01-05 15:34:07 -0300

    update

--- part1/tests/src/App.jsx
+++ part1/tests/src/App.jsx
@@ -1,41 +1,12 @@
 import { useState } from 'react'
 
-const Display = (props) => {
-  return (
-    <div>{props.counter}</div>
-  )
-}
-
-const Button = (props) => {
-  return (
-    <button onClick={props.onClick}>
-      {props.text}
-    </button>
-  )
-}
-
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const [value, setValue] = useState(10)
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      />           
+      {value}
+      <button>reset to zero</button>
     </div>
   )
 }
